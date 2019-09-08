77884fc29e6dedc5ca7dfc485827385353ea4aba
Stephen Thompson <smt3@hotmail.com>
2019-09-08 15:02:57 -0500

    added clear package

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
 var c = require('colors');
+var clear = require('clear');
 
 //create mySQL connection to bamazon
 var db = mysql.createConnection({
@@ -15,8 +16,6 @@ var db = mysql.createConnection({
 db.connect(function(err){
     if(err) throw err;
     showProducts();
-    //startBamazon();
-    //db.end();
 });
 
 function showProducts(){
@@ -24,12 +23,11 @@ function showProducts(){
         if (err) throw err;
         
         console.log('\r\n\n\n***WELCOME TO bAMAzON!****'.rainbow + '\r\n');
-        startBamazon();
         console.log('\r\nAvailable Products for Purchase:'.inverse.cyan.underline+'\r\n');
         console.table(res);
-        
+        startBamazon();
     })
-}
+};
 
 //start the Bamazon App
 function startBamazon(){
@@ -44,7 +42,7 @@ function startBamazon(){
             if (err) throw err;
             if (!res.length){
                 console.log('Item not in store or enter an ID on the list!'.red.inverse);
-                showProducts();
+                startBamazon();
             }
             else
             {
@@ -56,13 +54,14 @@ function startBamazon(){
     });
 };
 
+//Start Order
 function orderBamazon(itemID){
     var updQty;
 inquirer
 .prompt({
     name: 'askQty',
     type: 'input',
-    message: 'Enter enter the quantity you want to purchase: '
+    message: 'Enter enter the quantity you want to purchase, type EXIT to quit: '
 })
 .then(function(answer){
     db.query("SELECT stock_quantity FROM products WHERE item_id="+itemID+"", function(err,res){
@@ -75,16 +74,21 @@ inquirer
             orderBamazon(itemID);
         } else if (selQty === 0){
             console.log('Enter a number greater than zero (0)!'.inverse.red.underline);
+            clear();
             orderBamazon(itemID);
         } else if (selQty <= qtyVal ) {
             updQty = qtyVal - selQty;
             fulfillBamazon(updQty,selQty,itemID);
+        } else if(answer.askQty === 'EXIT'){
+            db.end;
+            clear();
+            process.exit();
         }
     }) 
-   
 })
 };
 
+//Process Order
 function fulfillBamazon(rmgQty,buyQty,itemID){
     db.query("UPDATE products SET stock_quantity="+rmgQty+" WHERE (item_id="+itemID+")", function(err,res){
         if (err) throw err;
@@ -93,7 +97,7 @@ function fulfillBamazon(rmgQty,buyQty,itemID){
         if (err) throw err;
         var itemPrice = res[0].price;
         var totalCost = itemPrice * buyQty;
-        console.log("Total cost of your purchase:".inverse.italic.underline.green + totalCost.toLocaleString('en-US',{style:'currency', currency:'USD'}));
+        console.log("\r\nThank you for your business. Your purchase amount:".inverse.italic.underline.green + totalCost.toLocaleString('en-US',{style:'currency', currency:'USD'})+'\r\n');
     });
     inquirer
     .prompt({
@@ -104,21 +108,25 @@ function fulfillBamazon(rmgQty,buyQty,itemID){
     .then(function(answer){
         switch(answer.askShop){
             case 'y':
-                startBamazon();
+                showProducts();
             break;
             case 'Y':
-                startBamazon();
+                showProducts();
             break;
             case 'n':
                 db.end;
+                clear();
                 process.exit();
             break;
             case 'N':
                 db.end;
+                clear();
                 process.exit();
             break;
             default:
+                db.end;
+                clear();
                 process.exit();
         }
     });
-}
+};
